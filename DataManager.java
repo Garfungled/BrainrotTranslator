23f4fc8eb2651893a84c101bf97db0a1aa973a27
FishDrowned <zach.medj55@gmail.com>
2025-01-03 12:24:33 -0500

    fixed getAllValues to not include a comma on the last value

--- DataManager.java
+++ DataManager.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 
 
 public class DataManager {
-   public static String csvFileName = "CSV/main.csv";
-   public static String valuesFileName = "CSV/values.txt";
+    public static String csvFileName = "CSV/main.csv";
+    public static String valuesFileName = "CSV/values.txt";
 
 
-   public static void updateValues() throws IOException {
-       FileWriter fileWriter = new FileWriter(valuesFileName);
-       fileWriter.write(getAllValues());
-       fileWriter.close();
-   }
+    public static void updateValues() throws IOException {
+        FileWriter fileWriter = new FileWriter(valuesFileName);
+        fileWriter.write(getAllValues());
+        fileWriter.close();
+    }
 
 
    // Returns all the values from the CSV in one long string (with no duplicates)
@@ -21,45 +21,43 @@ public class DataManager {
    public static String getAllValues() {
        String total = "";
        try {
-           Scanner csvScanner = new Scanner(new File(csvFileName));
-           while(csvScanner.hasNextLine()) {
-               String currentLine = csvScanner.nextLine();
-               total += currentLine.substring(currentLine.indexOf(',', currentLine.indexOf(',') + 1) + 1) + ",";
-           }
-           csvScanner.close();
-       } catch (Exception e) {
-           System.out.println("Error reading CSV");
-           System.out.println(e);
-       }
-
-
+            Scanner csvScanner = new Scanner(new File(csvFileName));
+            while(csvScanner.hasNextLine()) {
+                String currentLine = csvScanner.nextLine();
+                total += currentLine.substring(currentLine.indexOf(',', currentLine.indexOf(',') + 1) + 1) + ",";
+            }
+            csvScanner.close();
+        } catch (Exception e) {
+            System.out.println("Error reading CSV");
+            System.out.println(e);
+        }
       
-       return clearDuplicates(total);
+        return clearDuplicates(total).substring(0, total.length() - 1);
    }
 
 
    // input: "value1,value2,value3,...", output is the same
-   public static String clearDuplicates(String n) {
-       String result = "";
+    public static String clearDuplicates(String n) {
+        String result = "";
 
+        int currentCommaIndex = n.indexOf(',');
+        int lastCommaIndex = 0;
+        while(currentCommaIndex != -1) {
+            String currentValue = n.substring(lastCommaIndex, currentCommaIndex);
+            currentValue =  currentValue.replace(",", "");
 
-       int currentCommaIndex = n.indexOf(',');
-       int lastCommaIndex = 0;
-       while(currentCommaIndex != -1) {
-           String currentValue = n.substring(lastCommaIndex, currentCommaIndex);
-           currentValue =  currentValue.replace(",", "");
+            if (!result.contains(currentValue)) {
+                result += currentValue + ",";
+            }
 
+            lastCommaIndex = currentCommaIndex;
+            currentCommaIndex = n.indexOf(',', currentCommaIndex + 1);
+        }
 
-           if (!result.contains(currentValue)) {
-               result += currentValue + ",";
-           }
+        return result;
+    }
 
-
-           lastCommaIndex = currentCommaIndex;
-           currentCommaIndex = n.indexOf(',', currentCommaIndex + 1);
-       }
-
-
-       return result;
-   }
+    public static void main(String[] args) throws IOException {
+        updateValues();
+    }
 }
